ae0430dc8338b051260d32ec885e7b087d22e903
Raphael Medeiros <raphaelmedeiros2004@gmail.com>
2025-01-21 09:17:45 -0300

    First Commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = []
+let lista = document.getElementById('listaAmigos')
+
+
+function adicionarAmigo() {
+  let nome = document.querySelector('input').value
+  if (nome == '') {
+    alert('Insira um nome!')
+    return;
+  }
+
+  listaAmigos.push(nome)
+  lista.innerHTML += `<li>${nome}</li>`
+
+  document.querySelector('input').value = ''
+}
+
+
+function sortearAmigo() {
+  if (listaAmigos.length > 0) {
+
+    let numeroAleatorio = parseInt(Math.random() * listaAmigos.length)
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = `${listaAmigos[numeroAleatorio]}`
+
+    console.log(numeroAleatorio)
+
+  } else alert('Insira um nome para sortear!')
+}
